d93c92eff03ee85e892f112dfa5d59111adc7692
Alex Hung <alexh@jfrog.com>
2021-11-30 11:30:02 -0800

    Add Artifactory license check

--- pkg/projects/provider_test.go
+++ pkg/projects/provider_test.go
@@ -2,8 +2,10 @@ package projects
 
 import (
 	"context"
+	"fmt"
 	"github.com/go-resty/resty/v2"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
@@ -37,11 +39,16 @@ func getTestResty(t *testing.T) *resty.Client {
 
 func testAccPreCheck(t *testing.T) {
 	restyClient := getTestResty(t)
-	// TODO check the payload and make sure it's the right license type
-	_, err := restyClient.R().Get("/artifactory/api/system/licenses/")
+	resp, err := restyClient.R().Get("/artifactory/api/system/licenses/")
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	body := fmt.Sprintf("%s", resp.Body())
+	if !strings.Contains(body, "Enterprise") {
+		t.Fatal(body, "\nArtifactory requires Enterprise license to work with Terraform!")
+	}
+
 	ctx := context.Background()
 	provider, _ := testAccProviders["project"]()
 	oldErr := provider.Configure(ctx, terraform.NewResourceConfigRaw(nil))
