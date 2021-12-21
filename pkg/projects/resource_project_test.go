cfd3cf148dcae8e926047a314d6cbab82198bea7
Alex Hung <alexh@jfrog.com>
2021-12-21 14:51:47 -0800

    Add sub-resources to project test
    Ensure role is updated first as members and groups may depend on them

--- pkg/projects/resource_project_test.go
+++ pkg/projects/resource_project_test.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/go-resty/resty/v2"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
@@ -26,18 +27,18 @@ func makeInvalidProjectKeyTestCase(invalidProjectKey string, t *testing.T) (*tes
 	}
 	project := executeTemplate("TestAccProjects", `
 		resource "project" "{{ .name }}" {
-            key = "{{ .project_key }}"
-            display_name = "{{ .name }}"
-            description = "test description"
-            admin_privileges {
-                manage_members = {{ .manage_members }}
-                manage_resources = {{ .manage_resources }}
-                index_resources = {{ .index_resources }}
-            }
-            max_storage_in_gibibytes = {{ .max_storage_in_gibibytes }}
+			key = "{{ .project_key }}"
+			display_name = "{{ .name }}"
+			description = "test description"
+			admin_privileges {
+				manage_members = {{ .manage_members }}
+				manage_resources = {{ .manage_resources }}
+				index_resources = {{ .index_resources }}
+			}
+			max_storage_in_gibibytes = {{ .max_storage_in_gibibytes }}
 			block_deployments_on_limit = {{ .block_deployments_on_limit }}
-            email_notification = {{ .email_notification }}
-        }
+			email_notification = {{ .email_notification }}
+		}
 	`, params)
 
 	return t, resource.TestCase{
@@ -58,7 +59,7 @@ type testCase struct {
 	Value string
 }
 
-func TestAccProjectsInvalidProjectKey(t *testing.T) {
+func TestAccProjectInvalidProjectKey(t *testing.T) {
 	invalidProjectKeys := []testCase{
 		{
 			Name:  "TooShort",
@@ -97,18 +98,18 @@ func TestAccProjectInvalidDisplayName(t *testing.T) {
 	}
 	project := executeTemplate("TestAccProjects", `
 		resource "project" "{{ .name }}" {
-            key = "{{ .project_key }}"
-            display_name = "{{ .name }}"
-            description = "test description"
-            admin_privileges {
-                manage_members = {{ .manage_members }}
-                manage_resources = {{ .manage_resources }}
-                index_resources = {{ .index_resources }}
-            }
-            max_storage_in_gibibytes = {{ .max_storage_in_gibibytes }}
+			key = "{{ .project_key }}"
+			display_name = "{{ .name }}"
+			description = "test description"
+			admin_privileges {
+				manage_members = {{ .manage_members }}
+				manage_resources = {{ .manage_resources }}
+				index_resources = {{ .index_resources }}
+			}
+			max_storage_in_gibibytes = {{ .max_storage_in_gibibytes }}
 			block_deployments_on_limit = {{ .block_deployments_on_limit }}
-            email_notification = {{ .email_notification }}
-        }
+			email_notification = {{ .email_notification }}
+		}
 	`, params)
 
 	resource.Test(t, resource.TestCase{
@@ -128,6 +129,15 @@ func TestAccProject(t *testing.T) {
 	name := fmt.Sprintf("tftestprojects%s", randSeq(10))
 	resourceName := fmt.Sprintf("project.%s", name)
 
+	username1 := "user1"
+	email1 := username1 + "@tempurl.org"
+	username2 := "user2"
+	email2 := username2 + "@tempurl.org"
+	group1 := "group1"
+	group2 := "group2"
+	repo1 := "repo1"
+	repo2 := "repo2"
+
 	params := map[string]interface{}{
 		"max_storage_in_gibibytes":   rand.Intn(100),
 		"block_deployments_on_limit": randBool(),
@@ -137,26 +147,89 @@ func TestAccProject(t *testing.T) {
 		"index_resources":            randBool(),
 		"name":                       name,
 		"project_key":                strings.ToLower(randSeq(6)),
+		"username1":                  username1,
+		"username2":                  username2,
+		"group1":                     group1,
+		"group2":                     group2,
+		"repo1":                      repo1,
+		"repo2":                      repo2,
 	}
+
 	project := executeTemplate("TestAccProjects", `
 		resource "project" "{{ .name }}" {
-            key = "{{ .project_key }}"
-            display_name = "{{ .name }}"
-            description = "test description"
-            admin_privileges {
-                manage_members = {{ .manage_members }}
-                manage_resources = {{ .manage_resources }}
-                index_resources = {{ .index_resources }}
-            }
-            max_storage_in_gibibytes = {{ .max_storage_in_gibibytes }}
+			key = "{{ .project_key }}"
+			display_name = "{{ .name }}"
+			description = "test description"
+			admin_privileges {
+				manage_members = {{ .manage_members }}
+				manage_resources = {{ .manage_resources }}
+				index_resources = {{ .index_resources }}
+			}
+			max_storage_in_gibibytes = {{ .max_storage_in_gibibytes }}
 			block_deployments_on_limit = {{ .block_deployments_on_limit }}
-            email_notification = {{ .email_notification }}
-        }
+			email_notification = {{ .email_notification }}
+
+			member {
+				name  = "{{ .username1 }}"
+				roles = ["developer","project admin"]
+			}
+
+			member {
+				name  = "{{ .username2 }}"
+				roles = ["developer"]
+			}
+
+			group {
+				name  = "{{ .group1 }}"
+				roles = ["qa"]
+			}
+
+			group {
+				name  = "{{ .group2 }}"
+				roles = ["release manager"]
+			}
+
+			role {
+				name         = "qa"
+				description  = "QA role"
+				type         = "CUSTOM"
+				environments = ["DEV"]
+				actions      = ["READ_REPOSITORY","READ_RELEASE_BUNDLE", "READ_BUILD", "READ_SOURCES_PIPELINE", "READ_INTEGRATIONS_PIPELINE", "READ_POOLS_PIPELINE", "TRIGGER_PIPELINE"]
+			}
+
+			role {
+				name         = "devop"
+				description  = "DevOp role"
+				type         = "CUSTOM"
+				environments = ["DEV", "PROD"]
+				actions      = ["READ_REPOSITORY", "ANNOTATE_REPOSITORY", "DEPLOY_CACHE_REPOSITORY", "DELETE_OVERWRITE_REPOSITORY", "TRIGGER_PIPELINE", "READ_INTEGRATIONS_PIPELINE", "READ_POOLS_PIPELINE", "MANAGE_INTEGRATIONS_PIPELINE", "MANAGE_SOURCES_PIPELINE", "MANAGE_POOLS_PIPELINE", "READ_BUILD", "ANNOTATE_BUILD", "DEPLOY_BUILD", "DELETE_BUILD",]
+			}
+
+			repos = ["{{ .repo1 }}", "{{ .repo2 }}"]
+		}
 	`, params)
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:          func() { testAccPreCheck(t) },
-		CheckDestroy:      verifyDeleted(resourceName, verifyProject),
+		PreCheck: func() {
+			testAccPreCheck(t)
+			createTestUser(t, username1, email1)
+			createTestUser(t, username2, email2)
+			createTestGroup(t, group1)
+			createTestGroup(t, group2)
+			createTestRepo(t, repo1)
+			createTestRepo(t, repo2)
+		},
+		CheckDestroy: verifyDeleted(resourceName, func(id string, request *resty.Request) (*resty.Response, error) {
+			deleteTestUser(t, username1)
+			deleteTestUser(t, username2)
+			deleteTestGroup(t, group1)
+			deleteTestGroup(t, group2)
+			deleteTestRepo(t, repo1)
+			deleteTestRepo(t, repo2)
+			resp, err := verifyProject(id, request)
+
+			return resp, err
+		}),
 		ProviderFactories: testAccProviders(),
 		Steps: []resource.TestStep{
 			{
@@ -171,6 +244,24 @@ func TestAccProject(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "admin_privileges.0.manage_members", fmt.Sprintf("%t", params["manage_members"])),
 					resource.TestCheckResourceAttr(resourceName, "admin_privileges.0.manage_resources", fmt.Sprintf("%t", params["manage_resources"])),
 					resource.TestCheckResourceAttr(resourceName, "admin_privileges.0.index_resources", fmt.Sprintf("%t", params["index_resources"])),
+					resource.TestCheckResourceAttr(resourceName, "member.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "member.0.name", username1),
+					resource.TestCheckResourceAttr(resourceName, "member.0.roles.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "member.0.roles.0", "developer"),
+					resource.TestCheckResourceAttr(resourceName, "member.0.roles.1", "project admin"),
+					resource.TestCheckResourceAttr(resourceName, "member.1.name", username2),
+					resource.TestCheckResourceAttr(resourceName, "member.1.roles.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "member.1.roles.0", "developer"),
+					resource.TestCheckResourceAttr(resourceName, "group.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "group.0.name", group1),
+					resource.TestCheckResourceAttr(resourceName, "group.0.roles.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "group.0.roles.0", "qa"),
+					resource.TestCheckResourceAttr(resourceName, "group.1.name", group2),
+					resource.TestCheckResourceAttr(resourceName, "group.1.roles.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "group.1.roles.0", "release manager"),
+					resource.TestCheckResourceAttr(resourceName, "repos.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "repos.0", repo1),
+					resource.TestCheckResourceAttr(resourceName, "repos.1", repo2),
 				),
 			},
 		},
