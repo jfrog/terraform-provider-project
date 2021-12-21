d2fb0bae43e5371aa22c5f77ea20d62173799b5b
Alex Hung <alexh@jfrog.com>
2021-12-20 16:26:00 -0800

    Improve license checking

--- pkg/projects/provider.go
+++ pkg/projects/provider.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
-	"strings"
+	"regexp"
 
 	"github.com/go-resty/resty/v2"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -79,7 +79,7 @@ func buildResty(URL string) (*resty.Client, error) {
 	}).
 		SetHeader("content-type", "application/json").
 		SetHeader("accept", "*/*").
-		SetHeader("user-agent", "jfrog/terraform-provider-projects:"+Version).
+		SetHeader("user-agent", "jfrog/terraform-provider-project:"+Version).
 		SetRetryCount(5)
 
 	restyBase.DisableWarn = true
@@ -97,19 +97,32 @@ func addAuthToResty(client *resty.Client, accessToken string) (*resty.Client, er
 func checkArtifactoryLicense(client *resty.Client) error {
 
 	type License struct {
-		Type         string `json:"type"`
-		ValidThrough string `json:"validThrough"`
-		LicensedTo   string `json:"licensedTo"`
+		Type string `json:"type"`
 	}
 
-	license := License{}
-	_, err := client.R().SetResult(&license).Get("/artifactory/api/system/licenses/")
+	type LicensesWrapper struct {
+		License
+		Licenses []License `json:"licenses"` // HA licenses returns as an array instead
+	}
+
+	licensesWrapper := LicensesWrapper{}
+	_, err := client.R().
+		SetResult(&licensesWrapper).
+		Get("/artifactory/api/system/licenses")
+
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to check for license. %s", err)
 	}
 
-	if !strings.Contains(license.Type, "Enterprise") {
-		return fmt.Errorf("Artifactory requires Enterprise license to work with Terraform!")
+	var licenseType string
+	if len(licensesWrapper.Licenses) > 0 {
+		licenseType = licensesWrapper.Licenses[0].Type
+	} else {
+		licenseType = licensesWrapper.Type
+	}
+
+	if matched, _ := regexp.MatchString(`(?:Enterprise|Commercial)`, licenseType); !matched {
+		return fmt.Errorf("Artifactory requires Pro or Enterprise license to work with Terraform!")
 	}
 
 	return nil
